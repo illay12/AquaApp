48ebefffd36fa9aea2610b6c10b11d4b0f2fa473
illay12 <laurentiupetrica11@yahoo.com>
2026-03-23 09:50:01 +0200

    coep update

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -51,6 +51,7 @@ class SecurityHeaders
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
         $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+        $response->headers->set('Cross-Origin-Opener-Policy', 'same-origin');
 
         return $response;
     }
