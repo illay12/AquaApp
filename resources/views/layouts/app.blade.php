7cc162c6f68c8b1143d7c2c8d508f14aafaa4e82
illay12 <laurentiupetrica11@yahoo.com>
2026-05-19 11:11:47 +0300

    statistics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -146,6 +146,16 @@
     </style>
 
     @stack('styles')
+
+    @if(env('GOOGLE_ANALYTICS_ID'))
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '{{ env('GOOGLE_ANALYTICS_ID') }}');
+    </script>
+    @endif
 </head>
 <body>
 
